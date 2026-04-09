c68a0ccc8d03871259a98c1aaf9b6b556d59d8d2
ADYANTO WAHYUDHI NUGROHO <50142260+adyantown@users.noreply.github.com>
2026-04-09 15:31:08 +0700

    Remove unused path import from upload middleware
    Remove the unused `path` require from middleware/upload.js. Cleans up dead code and prevents linter warnings since the path module was not referenced in the file.

--- middleware/upload.js
+++ middleware/upload.js
@@ -1,7 +1,6 @@
 const multer = require('multer');
 const { CloudinaryStorage } = require('multer-storage-cloudinary');
 const cloudinary = require('cloudinary').v2;
-const path = require('path');
 
 // Konfigurasi Cloudinary
 cloudinary.config({
